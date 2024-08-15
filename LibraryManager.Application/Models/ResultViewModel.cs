abb6ca897cbd2eb1e263e4e9c7709d4bab5333c8
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-08-14 22:02:07 -0300

    Novo commit

---
+++ LibraryManager.Application/Models/ResultViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Manager.Application.Models
+{
+    public class ResultViewModel
+    {
+        public ResultViewModel(bool isSuccess = true, string message = "")
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+    }
+}
